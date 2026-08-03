5b856478074d49436b678d662865f1ccf8b0df73
Sharanya S Parol <sharanya.parol@incture.com>
2026-08-03 04:45:58 +0000

    Remove dist folder from repository

--- dist/controller/App.controller.js
+++
@@ -1,2 +0,0 @@
-sap.ui.define(["./BaseController"],function(n){"use strict";return n.extend("inventoryportal.controller.App",{onInit:function(){}})});
-//# sourceMappingURL=App.controller.js.map
--- dist/controller/NotFound.controller.js
+++
@@ -1,2 +0,0 @@
-sap.ui.define(["./BaseController"],function(t){"use strict";return t.extend("inventoryportal.controller.NotFound",{onNavBack:function(){this.getRouter().navTo("list")}})});
-//# sourceMappingURL=NotFound.controller.js.map
